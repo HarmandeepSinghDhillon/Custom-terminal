7e82e2e9fbcd144b05c8b3e720039051641511f8
Harman <harmandeep.singh233@lpu.in>
2025-04-13 19:17:54 +0530

    second commit

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="custom-terminal",  # PyPI package name (must be unique)
-    version="0.1.0",        # Update for each release
-    packages=find_packages(),
-    install_requires=["requests"],  # List dependencies
+    name="custom-terminal",
+    version="0.1.0",
+    packages=["custom_terminal"],
+    install_requires=["requests"],
     entry_points={
-        'console_scripts': [
-            'cterm=custom_terminal.main:main',  # CLI command
-        ],
+        'console_scripts': ['cterm=custom_terminal.main:main'],
     },
-    author="Harmandeep Singh",
-    description="A custom terminal with weather commands",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/HarmandeepSinghDhillon/Custom-Terminal.git",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
 )
